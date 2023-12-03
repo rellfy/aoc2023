cf54407c3c571fcbc02628dd4c5498d706227b10
Lorenzo <lorenzo@nioel.com>
2023-12-03 20:45:53 +1000

    feat: setup day 3

---
+++ src/day3/part2.rs
@@ -0,0 +1,3 @@
+pub fn solve(input: &str) -> u64 {
+    0
+}
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,14 @@
 mod day1;
 #[cfg(feature = "day2")]
 mod day2;
+#[cfg(feature = "day3")]
+mod day3;
 
 fn main() {
     #[cfg(feature = "day1")]
     day1::run();
     #[cfg(feature = "day2")]
     day2::run();
+    #[cfg(feature = "day3")]
+    day3::run();
 }
