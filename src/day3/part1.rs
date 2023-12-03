025c99d80428a8724706cef6605ea3dd4c009314
Lorenzo <lorenzo@nioel.com>
2023-12-03 21:12:17 +1000

    feat(day3): get complete number at index

--- src/day3/part1.rs
+++ src/day3/part1.rs
@@ -1,3 +1,26 @@
 pub fn solve(input: &str) -> u64 {
     0
 }
+
+fn get_complete_number_at_index(line: &str, index: usize) -> u64 {
+    // Get adjacent digits from start to index.
+    let mut backward_chars = line.chars().rev().skip(line.len() - index);
+    let mut backward_digits = String::new();
+    while let Some(c) = backward_chars.next() {
+        if !c.is_ascii_digit() {
+            break;
+        }
+        backward_digits.push(c);
+    }
+    backward_digits = backward_digits.chars().rev().collect();
+    // Get adjacent digits from index to end.
+    let mut forward_chars = line.chars().skip(index);
+    let mut forward_digits = String::new();
+    while let Some(c) = forward_chars.next() {
+        if !c.is_ascii_digit() {
+            break;
+        }
+        forward_digits.push(c);
+    }
+    format!("{backward_digits}{forward_digits}").parse().unwrap()
+}
