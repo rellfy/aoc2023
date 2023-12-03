bf021ee269af95becf0d3cddae58bb060b04171d
Lorenzo <lorenzo@nioel.com>
2023-12-03 16:46:05 +1000

    refactor: day 1 part 1

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,35 +1,7 @@
-static INPUT: &str = include_str!("input.txt");
+mod part1;
+
+pub static INPUT: &str = include_str!("input.txt");
 
 pub fn run() {
-    let result = solve(INPUT);
-    println!("result: {result}");
-}
-
-fn solve(input: &str) -> u64 {
-    input.lines().map(|line| get_line_number(line)).sum()
-}
-
-fn get_line_number(line: &str) -> u64 {
-    let first_digit = get_first_digit(line);
-    let last_digit = get_last_digit(line);
-    let number_str = format!("{first_digit}{last_digit}");
-    number_str.parse().unwrap()
-}
-
-fn get_first_digit(line: &str) -> char {
-    get_digit(line.chars())
-}
-
-fn get_last_digit(line: &str) -> char {
-    get_digit(line.chars().rev())
-}
-
-fn get_digit<I>(iter: I) -> char where I: Iterator<Item = char> {
-    for c in iter {
-        if !c.is_ascii_digit() {
-            continue;
-        }
-        return c;
-    }
-    panic!("no digit found");
+    println!("part 1 result: {}", part1::solve(INPUT));
 }
---
+++ src/day1/part1.rs
@@ -0,0 +1,28 @@
+pub fn solve(input: &str) -> u64 {
+    input.lines().map(|line| get_line_number(line)).sum()
+}
+
+fn get_line_number(line: &str) -> u64 {
+    let first_digit = get_first_digit(line);
+    let last_digit = get_last_digit(line);
+    let number_str = format!("{first_digit}{last_digit}");
+    number_str.parse().unwrap()
+}
+
+fn get_first_digit(line: &str) -> char {
+    get_digit(line.chars())
+}
+
+fn get_last_digit(line: &str) -> char {
+    get_digit(line.chars().rev())
+}
+
+fn get_digit<I>(iter: I) -> char where I: Iterator<Item = char> {
+    for c in iter {
+        if !c.is_ascii_digit() {
+            continue;
+        }
+        return c;
+    }
+    panic!("no digit found");
+}
