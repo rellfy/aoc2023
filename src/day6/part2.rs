6983152560d1cf4e8d045f7e8ff5f56a3c9abd38
Lorenzo Fontoura <lorenzo@nioel.com>
2023-12-09 14:34:16 +1000

    refactor: reduce duplication

--- src/day6/part2.rs
+++ src/day6/part2.rs
@@ -2,15 +2,16 @@ use crate::day::part1::calculate_win_possibilities;
 
 pub fn solve(input: &str) -> u64 {
     let mut lines = input.lines();
-    let time = lines
-        .next().unwrap().chars().skip(11).collect::<String>()
-        .replace(' ', "")
-        .parse::<i64>()
-        .unwrap();
-    let distance = lines
-        .next().unwrap().chars().skip(11).collect::<String>()
-        .replace(' ', "")
-        .parse::<i64>()
-        .unwrap();
+    let time = parse_line(lines.next().unwrap());
+    let distance = parse_line(lines.next().unwrap());
     calculate_win_possibilities(time, distance) as u64
+}
+
+fn parse_line(line: &str) -> i64 {
+    line.chars()
+        .skip(11)
+        .collect::<String>()
+        .replace(' ', "")
+        .parse::<i64>()
+        .unwrap()
 }
