f9d911e43901ec8c7b958c40bdcf931817312bb1
Lorenzo Fontoura <lorenzo@nioel.com>
2023-12-09 14:33:14 +1000

    feat: day 6 part 2

--- src/day6/part2.rs
+++ src/day6/part2.rs
@@ -1,4 +1,16 @@
+use crate::day::part1::calculate_win_possibilities;
 
 pub fn solve(input: &str) -> u64 {
-    0
+    let mut lines = input.lines();
+    let time = lines
+        .next().unwrap().chars().skip(11).collect::<String>()
+        .replace(' ', "")
+        .parse::<i64>()
+        .unwrap();
+    let distance = lines
+        .next().unwrap().chars().skip(11).collect::<String>()
+        .replace(' ', "")
+        .parse::<i64>()
+        .unwrap();
+    calculate_win_possibilities(time, distance) as u64
 }
