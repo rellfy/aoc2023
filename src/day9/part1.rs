2bbcb5791d9b47b9344aceff1a3ffd40a5e0e590
Lorenzo Fontoura <lorenzo@nioel.com>
2023-12-09 15:07:02 +1000

    chore: setup

--- src/day9/part1.rs
+++ src/day9/part1.rs
@@ -1,4 +1,12 @@
 
 pub fn solve(input: &str) -> u64 {
+    input
+        .lines()
+        .map(|line| line.split(' ').map(|v| v.parse::<u64>().unwrap()).collect::<Vec<u64>>())
+        .map(|values| find_next_value(&values))
+        .sum()
+}
+
+fn find_next_value(values: &[u64]) -> u64 {
     0
 }
